8af6cd2294273b1d4cab13c294dc2bc00a9139db
Hans Scharler <hscharler@gmail.com>
2019-01-03 22:25:35 -0500

    Updated examples to use the official ThingSpeak library

--- examples/A0_to_ThingSpeak.ino
+++ examples/A0_to_ThingSpeak.ino
@@ -1,5 +1,5 @@
 /*
- ESP8266 --> ThingSpeak Channel via MKR1000 Wi-Fi
+ ESP8266 --> ThingSpeak Channel
  
  This sketch sends the value of Analog Input (A0) to a ThingSpeak channel
  using the ThingSpeak API (https://www.mathworks.com/help/thingspeak).
@@ -7,75 +7,77 @@
  Requirements:
  
    * ESP8266 Wi-Fi Device
-   * Arduino 1.6.9+ IDE
+   * Arduino 1.8.8+ IDE
    * Additional Boards URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
    * Library: esp8266 by ESP8266 Community
+   * Library: ThingSpeak by MathWorks
  
  ThingSpeak Setup:
  
    * Sign Up for New User Account - https://thingspeak.com/users/sign_up
    * Create a new Channel by selecting Channels, My Channels, and then New Channel
    * Enable one field
-   * Note the Channel ID and Write API Key
-    
+   * Enter SECRET_CH_ID in "secrets.h"
+   * Enter SECRET_WRITE_APIKEY in "secrets.h"
+
  Setup Wi-Fi:
-  * Enter SSID
-  * Enter Password
+  * Enter SECRET_SSID in "secrets.h"
+  * Enter SECRET_PASS in "secrets.h"
   
  Tutorial: http://nothans.com/measure-wi-fi-signal-levels-with-the-esp8266-and-thingspeak
    
  Created: Feb 3, 2017 by Hans Scharler (http://nothans.com)
 */
 
+#include "ThingSpeak.h"
+#include "secrets.h"
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
 #include <ESP8266WiFi.h>
 
-// Wi-Fi Settings
-const char* ssid = "XXX"; // your wireless network name (SSID)
-const char* password = "XXXXXXXX"; // your Wi-Fi network password
-
-WiFiClient client;
-
-// ThingSpeak Settings
-const int channelID = XXX;
-String writeAPIKey = "XXXXXXXXXXXXXXXX"; // write API key for your ThingSpeak Channel
-const char* server = "api.thingspeak.com";
-const int postingInterval = 20 * 1000; // post data every 20 seconds
+char ssid[] = SECRET_SSID;   // your network SSID (name)
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key index number (needed only for WEP)
+WiFiClient  client;
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  delay(100);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  WiFi.mode(WIFI_STA);
+
+  ThingSpeak.begin(client);
 }
 
 void loop() {
-  if (client.connect(server, 80)) {
-    
-    // Measure Analog Input (A0)
-    int valueA0 = analogRead(A0);
 
-    // Construct API request body
-    String body = "field1=";
-           body += String(valueA0);
-    
-    Serial.print("A0: ");
-    Serial.println(valueA0); 
-
-    client.println("POST /update HTTP/1.1");
-    client.println("Host: api.thingspeak.com");
-    client.println("User-Agent: ESP8266 (nothans)/1.0");
-    client.println("Connection: close");
-    client.println("X-THINGSPEAKAPIKEY: " + writeAPIKey);
-    client.println("Content-Type: application/x-www-form-urlencoded");
-    client.println("Content-Length: " + String(body.length()));
-    client.println("");
-    client.print(body);
-
+  // Connect or reconnect to WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
   }
-  client.stop();
 
-  // wait and then post again
-  delay(postingInterval);
+  // Measure Analog Input (A0)
+  int valueA0 = analogRead(A0);
+
+  // Write value to Field 1 of a ThingSpeak Channel
+  int httpCode = ThingSpeak.writeField(myChannelNumber, 1, valueA0, myWriteAPIKey);
+
+  if (httpCode == 200) {
+    Serial.println("Channel write successful.");
+  }
+  else {
+    Serial.println("Problem writing to channel. HTTP error code " + String(httpCode));
+  }
+
+  // Wait 20 seconds to uodate the channel again
+  delay(20000);
 }
