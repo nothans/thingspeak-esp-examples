0f41e1f3b2668f364e0b20c4900676b95ad3458d
Hans Scharler <hscharler@gmail.com>
2019-01-03 21:40:59 -0500

    Update secrets.h

--- examples/secrets.h
+++ examples/secrets.h
@@ -5,4 +5,4 @@
 #define SECRET_PASS "MyPassword"                            // replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 000000                                 // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "XYZ"                           // replace XYZ with your channel write API Key
+#define SECRET_WRITE_APIKEY "123"                           // replace XYZ with your channel write API Key
